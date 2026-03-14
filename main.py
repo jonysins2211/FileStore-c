030d1909ca98a0f85a264e1c5bd032211fc898cd
jonysins2211 <Jonysins11220@gmail.com>
2026-03-14 13:55:34 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 from bot import Bot
 import pyrogram.utils
+import threading
+from masked_server import run_server
 
 pyrogram.utils.MIN_CHANNEL_ID = -1009147483647
 
 if __name__ == "__main__":
+
+    # start masked link server
+    threading.Thread(target=run_server).start()
+
+    # start telegram bot
     Bot().run()
